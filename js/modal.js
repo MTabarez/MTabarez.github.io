47899909c0b825b842417bff3f8b460b2e461636
Marianne Tabarez <33162419+MTabarez@users.noreply.github.com>
2018-11-18 13:37:17 -0300

    actualización del sábado

---
+++ js/modal.js
@@ -0,0 +1,127 @@
+              
+              var cantidad = 1, precio = 59, impuesto = 1.22, envio = 0, precioFinal = 0;
+                  
+                  function mostrarEsPaypal(){
+                    document.getElementById("esTarjeta").style.display = 'none';
+                    document.getElementById("esPaypal").style.display = 'block';
+                    document.getElementById("radioEsTarjeta").checked = false;
+                  }
+                  function mostrarEsTarjeta(){
+                    document.getElementById("esTarjeta").style.display = 'block';
+                    document.getElementById("esPaypal").style.display = 'none';
+                    document.getElementById("radioEsPaypal").checked = false;
+                  }
+                  function mostrarEsDigital(){
+                    document.getElementById("esFisico").style.display = 'none';
+                    document.getElementById("radioEsFisico").checked = false;
+                  }
+                  function mostrarEsFisico(){
+                    document.getElementById("esFisico").style.display = 'block';
+                    document.getElementById("radioEsDigital").checked = false;
+                  }
+                  function recalcularPrecioFinal(){
+                    precioFinal = ((precio * cantidad) + envio ) * impuesto ;
+                    document.getElementById("labelPrecioFinal").innerHTML = precioFinal.toFixed(2);
+                  }
+                  function aumentarCantidad(){
+                    cantidad++;
+                    document.getElementById("labelCantidad").innerHTML = cantidad;
+                    recalcularPrecioFinal();
+                  }
+                  function reducirCantidad(){
+                    if(cantidad > 1){
+                      cantidad--;
+                    }
+                    document.getElementById("labelCantidad").innerHTML = cantidad;
+                    recalcularPrecioFinal();
+                  }
+                  
+                  document.addEventListener("DOMContentLoaded",
+                  function(){
+                    //Digital o físico
+                    var radioEsDigital = document.getElementById("radioEsDigital");
+                    var radioEsFisico = document.getElementById("radioEsFisico");
+                    var esFisico = document.getElementById("esFisico");
+                    var esFisico = document.getElementById("esFisico");
+                  
+                    //Medio de pago
+                    var radioEsTarjeta = document.getElementById("radioEsTarjeta");
+                    var radioEsPaypal = document.getElementById("radioEsPaypal");
+                  
+                    //Tipo de envío
+                    var comboTipoEnvio = document.getElementById("comboTipoEnvio");
+                  
+                    //Detalle de pago
+                    var labelPrecio = document.getElementById("labelPrecio");
+                    var labelCantidad = document.getElementById("labelCantidad");
+                    var labelEnvio = document.getElementById("labelEnvio");
+                    var labelImpuesto = document.getElementById("labelImpuesto");
+                    var labelPrecioFinal = document.getElementById("labelPrecioFinal");
+                  
+                    labelPrecio.innerHTML = precio;
+                    labelCantidad.innerHTML = cantidad;
+                    labelEnvio.innerHTML = envio;
+                  
+                    recalcularPrecioFinal();
+                    labelPrecioFinal.innerHTML = precioFinal;
+                  
+                    //Cantidad
+                    var botonAumentarCantidad = document.getElementById("botonAumentarCantidad");
+                    var botonReducirCantidad = document.getElementById("botonReducirCantidad");
+                  
+                    //Listeners
+                    radioEsTarjeta.addEventListener("click", mostrarEsTarjeta);
+                    radioEsPaypal.addEventListener("click", mostrarEsPaypal);
+                  
+                    radioEsDigital.addEventListener("click", mostrarEsDigital);
+                    radioEsFisico.addEventListener("click", mostrarEsFisico);
+                  
+                    botonAumentarCantidad.addEventListener("click", aumentarCantidad);
+                    botonReducirCantidad.addEventListener("click", reducirCantidad);
+                  
+                    comboTipoEnvio.addEventListener("change", function() {
+                        
+                        switch(comboTipoEnvio.value) {
+                          case 'comun':
+                              envio = 0.005 * precio;
+                              break;
+                          case 'especial':
+                              envio = 0.02  * precio;
+                              break;
+                          case 'premium':
+                              envio = 0.05  * precio;
+                              break;
+                          default:
+                            envio = 0;
+                      }
+                      labelEnvio.innerHTML = envio;
+                      recalcularPrecioFinal();
+                    });
+                  
+                  }
+                  )
+                  
+
+         var modal = document.getElementById('myModal');
+         
+      
+         var btn = document.getElementById("myBtn");
+         
+         var span = document.getElementsByClassName("close")[0];
+         
+        
+         btn.onclick = function() {
+             modal.style.display = "block";
+         }
+         
+         
+         span.onclick = function() {
+             modal.style.display = "none";
+         }
+         
+         
+         window.onclick = function(event) {
+             if (event.target == modal) {
+                 modal.style.display = "none";
+             }
+         }
---
+++ js/script.js
@@ -0,0 +1,84 @@
+/* --------------------------MENÚ HAMBURGUESA------------------------------------- */
+
+function abrirMenu() {
+	var x = document.getElementById("navegacion");
+	if (x.className === "navstyle") {
+		x.className += " responsive";
+	} else {
+		x.className = "navstyle";
+	}
+}
+
+/* --------------------------SLIDER INDEX------------------------------------- */
+
+var slideIndex = 1;
+mostrarSlides(slideIndex);
+
+function cambioSlides(n) {
+	mostrarSlides(slideIndex += n);
+}
+
+function slideActual(n) {
+	mostrarSlides(slideIndex = n);
+}
+
+function mostrarSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("sliderhome");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}    
+		if (n < 1) {slideIndex = slides.length}
+			for (i = 0; i < slides.length; i++) {
+				slides[i].style.display = "none";  
+			}
+			for (i = 0; i < dots.length; i++) {
+				dots[i].className = dots[i].className.replace(" active", "");
+			}
+			slides[slideIndex-1].style.display = "block";  
+			dots[slideIndex-1].className += " active";
+		}
+
+/* --------------------------SLIDER DETALLE JUEGOS------------------------------------- */
+
+var slideJuegos = 1;
+mostrarSlidesJuegos(slideJuegos);
+
+function cambioSlidesJuegos(n) {
+	mostrarSlidesJuegos(slideJuegos += n);
+}
+
+function slideActualJuegos(n) {
+	mostrarSlidesJuegos(slideJuegos = n);
+}
+
+function mostrarSlidesJuegos(n) {
+	var i;
+	var slides = document.getElementsByClassName("sliderJuegos");
+	var dots = document.getElementsByClassName("dotJuegos");
+	if (n > slides.length) {slideJuegos = 1}    
+		if (n < 1) {slideJuegos = slides.length}
+			for (i = 0; i < slides.length; i++) {
+				slides[i].style.display = "none";  
+			}
+			for (i = 0; i < dots.length; i++) {
+				dots[i].className = dots[i].className.replace(" active", "");
+			}
+			slides[slideJuegos-1].style.display = "block";  
+			dots[slideJuegos-1].className += " active";
+		}
+
+/* --------------------------LEER MÁS------------------------------------- */
+
+function leerMas() {
+    var x = document.getElementById("leermas");
+    var botonMostrarMas = document.getElementById("botonleermas");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        botonMostrarMas.style.display = "none";
+    } else {
+        x.style.display = "none";
+        botonMostrarMas.style.display = "block";
+    }
+}
+
+/* --------------------------LEER MÁS CONTENIDO------------------------------------- */
