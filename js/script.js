ec0072068ca4fee851324d559bae63b6e27b43bd
Marianne Tabarez <33162419+MTabarez@users.noreply.github.com>
2018-11-21 01:31:06 -0300

    Arreglo página detalles

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 /* --------------------------MENÚ HAMBURGUESA------------------------------------- */
-
 function abrirMenu() {
-	var x = document.getElementById("navegacion");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
+    var x = document.getElementById("navegacion");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 /* --------------------------SLIDER INDEX------------------------------------- */
@@ -15,28 +14,32 @@ var slideIndex = 1;
 mostrarSlides(slideIndex);
 
 function cambioSlides(n) {
-	mostrarSlides(slideIndex += n);
+    mostrarSlides(slideIndex += n);
 }
 
 function slideActual(n) {
-	mostrarSlides(slideIndex = n);
+    mostrarSlides(slideIndex = n);
 }
 
 function mostrarSlides(n) {
-	var i;
-	var slides = document.getElementsByClassName("sliderhome");
-	var dots = document.getElementsByClassName("dot");
-	if (n > slides.length) {slideIndex = 1}    
-		if (n < 1) {slideIndex = slides.length}
-			for (i = 0; i < slides.length; i++) {
-				slides[i].style.display = "none";  
-			}
-			for (i = 0; i < dots.length; i++) {
-				dots[i].className = dots[i].className.replace(" active", "");
-			}
-			slides[slideIndex-1].style.display = "block";  
-			dots[slideIndex-1].className += " active";
-		}
+    var i;
+    var slides = document.getElementsByClassName("sliderhome");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
 
 /* --------------------------SLIDER DETALLE JUEGOS------------------------------------- */
 
@@ -44,28 +47,32 @@ var slideJuegos = 1;
 mostrarSlidesJuegos(slideJuegos);
 
 function cambioSlidesJuegos(n) {
-	mostrarSlidesJuegos(slideJuegos += n);
+    mostrarSlidesJuegos(slideJuegos += n);
 }
 
 function slideActualJuegos(n) {
-	mostrarSlidesJuegos(slideJuegos = n);
+    mostrarSlidesJuegos(slideJuegos = n);
 }
 
 function mostrarSlidesJuegos(n) {
-	var i;
-	var slides = document.getElementsByClassName("sliderJuegos");
-	var dots = document.getElementsByClassName("dotJuegos");
-	if (n > slides.length) {slideJuegos = 1}    
-		if (n < 1) {slideJuegos = slides.length}
-			for (i = 0; i < slides.length; i++) {
-				slides[i].style.display = "none";  
-			}
-			for (i = 0; i < dots.length; i++) {
-				dots[i].className = dots[i].className.replace(" active", "");
-			}
-			slides[slideJuegos-1].style.display = "block";  
-			dots[slideJuegos-1].className += " active";
-		}
+    var i;
+    var slides = document.getElementsByClassName("sliderJuegos");
+    var dots = document.getElementsByClassName("dotJuegos");
+    if (n > slides.length) {
+        slideJuegos = 1
+    }
+    if (n < 1) {
+        slideJuegos = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideJuegos - 1].style.display = "block";
+    dots[slideJuegos - 1].className += " active";
+}
 
 /* --------------------------LEER MÁS------------------------------------- */
 
